00aa46661012e400bb28f0abfe37038b45b9e0ac
Aurelio Joseph <aurelioaborges@gmail.com>
2023-01-13 06:47:11 -0300

    fixed typos

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd dist
 
 # place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
+echo .nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<aureliojoseph>/<devMusicPlayer>.git main:gh-pages
+git push -f git@github.com:aureliojoseph/devMusicPlayer.git main:gh-pages
 
 cd -
